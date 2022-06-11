b6f1bc367e22358e3c19783135d148c13c0ecaa4
symensjeffrey <jeffrey.symens23@gmail.com>
2022-06-10 20:39:50 -0500

    add url with working request

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    url = https://api.spoonacular.com/food/ingredients/search?query=a
-    render json: items
+    url = "https://api.spoonacular.com/food/ingredients/search?query=apple"
+    headers = {
+      "x-api-key" => "" 
+    }
+    resposne = RestClient.get(url, headers)
+    render json: resposne
   end
 end
