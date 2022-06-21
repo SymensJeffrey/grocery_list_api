b4c336ddf1bf2d108b3f51b5c16f9403c5446a90
symensjeffrey <jeffrey.symens23@gmail.com>
2022-06-21 17:31:06 -0500

    add list controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_004104) do
+ActiveRecord::Schema.define(version: 2022_06_21_223035) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
